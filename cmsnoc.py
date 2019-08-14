ef7f1ec63075857cadafeaae068a139063418c51
Aravind Sudarshan Ananthapadmanabhan (araviana) <araviana@cisco.com>
2019-08-14 19:08:29 +0530

    Made Login and corresponding object in backend

--- cmsnoc.py
+++ cmsnoc.py
@@ -31,8 +31,80 @@ mongo = MongoClient('mongodb+srv://cmsnoc93:'+ urllib.parse.quote('cmsnoc@123') 
 db = mongo.CMS_Automation
 collection = db.Ticket_automation
 app = Flask(__name__)
+app.secret_key = 'some_secret'
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    m = dict()
+    login_det = db.Login_table.find()
+    time = ''
+    if datetime.datetime.today().strftime('%p') == 'AM':
+        time = 'Morning'
+    elif datetime.datetime.today().strftime('%p') == 'PM':
+        time = 'Night'
+    shift_det = collection.find({"date":datetime.datetime.today().strftime('%d-%m-%Y'),'shift':time,'freeze':0})
+
+    for l in login_det:
+        m[l['username']] = l['password']
+    if request.method == 'GET':
+        return render_template('login.html')
+    if request.method == 'POST':
+        if (request.form['username'] in m.keys()):
+            if (m[request.form['username']] == request.form['password']):
+                session['logged_in'] = True
+                session['username'] = request.form['username']
+                if shift_det.count() == 0:
+                    engineers = {}
+                    engineers[request.form['username']] = dict()
+                    engineers[request.form['username']]['name'] = request.form['username']
+                    engineers[request.form['username']]['role'] = request.form['role']
+                    engineers[request.form['username']]['cases'] = []
+                    engineers[request.form['username']]['leave'] = 0
+                    print(engineers)
+                    collection.insert({"date": datetime.datetime.today().strftime('%d-%m-%Y'), 'shift': time, 'engineers':engineers, 'freeze':0})
+                elif shift_det.count()>0:
+                    engineers = {}
+                    for i in shift_det:
+                        engineers = i['engineers']
+                    engineers[request.form['username']] = dict()
+                    engineers[request.form['username']]['name'] = request.form['username']
+                    engineers[request.form['username']]['role'] = request.form['role']
+                    engineers[request.form['username']]['cases'] = []
+                    engineers[request.form['username']]['leave'] = 0
+                    myquery = {"date":datetime.datetime.today().strftime('%d-%m-%Y'),'shift':time,'freeze':0}
+                    newvalues = {"$set": {"engineers":engineers}}
+                    collection.update_one(myquery,newvalues)
+                return redirect(url_for('test'))# function name not endpoint name
+            else:
+                flash("Invalid Password", 'log_msg')
+                return redirect(url_for('login'))
+        else:
+            error = "Invalid Credentials"
+            flash(error, 'log_msg')
+    return redirect(url_for('login'))  # function name of the /attendance ednpoint
+
+@app.route('/logout',methods=['GET'])
+def logout():
+    time = ''
+    if datetime.datetime.today().strftime('%p') == 'AM':
+        time = 'Morning'
+    elif datetime.datetime.today().strftime('%p') == 'PM':
+        time = 'Night'
+    username = session['username']
+    shift_det = collection.find({"date": datetime.datetime.today().strftime('%d-%m-%Y'), 'shift': time, 'freeze': 0})
+    engineers = {}
+    for i in shift_det:
+        engineers = i['engineers']
+    del engineers[username]
+    myquery = {"date": datetime.datetime.today().strftime('%d-%m-%Y'), 'shift': time, 'freeze': 0}
+    newvalues = {"$set": {"engineers": engineers}}
+    collection.update_one(myquery, newvalues)
+    session['logged_in'] = False
+    flash(u'You were successfully logged  out!', 'log_msg')
+    return login()
 
 @app.route('/test', methods=['GET', 'POST'])
 def test():
-    collection.insert({'student_id': 12345})
-    return "Hi"
+    #collection.insert({'student_id': 12345})
+    print(datetime.datetime.today().strftime('%d-%m-%Y %p'))
+    return "Hi  " + session['username'] + "<form action = 'logout' method = 'get'><input type='submit' value = 'Logout'/></form>"
